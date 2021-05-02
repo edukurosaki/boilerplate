7c71bf2de4e4b7c733d3c136b8f02084b8ebcf9c
Eduardo Francisco <kurosaki.yslan@gmail.com>
2021-05-01 22:29:09 -0300

    atualizando test e storybook

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -3,7 +3,7 @@ const Main = () => (
   <S.Wrapper>
     <S.Logo
       src="/img/logo.svg"
-      alt="Img atomo e React Avançado escrito ao lado"
+      alt="Img atomo e React Avançado escrito ao ladoa"
     />
     <S.Title>React Avançado</S.Title>
     <S.Description>
