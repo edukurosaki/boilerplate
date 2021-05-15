dd29b21e56b115f08d195819d96abac47c617866
Eduardo Francisco <kurosaki.yslan@gmail.com>
2021-05-15 19:08:28 -0300

    Configurando nob essencials

--- src/components/Main/stories.tsx
+++ src/components/Main/stories.tsx
@@ -1,19 +1,19 @@
-import { withKnobs, text } from '@storybook/addon-knobs'
 import Main from '.'
 import { Meta, Story } from '@storybook/react/types-6-0'
 
 export default {
   title: 'Main',
-  components: Main,
-  decorators: [withKnobs]
+  components: Main
 } as Meta
 
-export const Basic: Story = () => (
-  <Main
-    title={text('Title', 'React Avançado')}
-    description={text(
-      'Description',
-      'TypeScript, ReactJS, NextJS e Styled Components'
-    )}
-  />
-)
+export const Basic: Story = (args) => <Main {...args} />
+Basic.args = {
+  title: 'titulo',
+  description: 'descrição'
+}
+
+export const Default: Story = (args) => <Main {...args} />
+Default.args = {
+  title: '123',
+  description: 'descrição002'
+}
